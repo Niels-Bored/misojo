93db5e9ad4d9ea1d6c971fe7268ec85d6aa3f82c
generic-projects <abelsotovaldez@gmail.com>
2022-12-30 21:54:59 -0600

    First version

---
+++ app.py
@@ -0,0 +1,126 @@
+import os
+import io
+from pdfmaker_Texas.PDFMaker import generatePDF as pdfTexas 
+from mysql import MySQL
+from functools import wraps
+from flask import Flask, render_template, request, redirect, url_for, session, send_file
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_HOST = os.environ['DATABASE_HOST']
+DATABASE_NAME = os.environ['DATABASE_NAME']
+DATABASE_USER = os.environ['DATABASE_USER']
+DATABASE_PASSWORD = os.environ['DATABASE_PASSWORD']
+
+Database = MySQL(DATABASE_HOST, DATABASE_NAME, DATABASE_USER, DATABASE_PASSWORD)
+
+app = Flask(__name__)
+app.secret_key = b'bb,hjd87li7fiopsddfrgt5'
+
+def validate_session():
+    def wraper(function):
+        @wraps(function)
+        def decorator(*args, **kwargs):
+            user=session.get("user","")
+            if user:
+                return function(*args, **kwargs)
+            else:
+                return redirect(url_for("login"))
+        return decorator
+    return wraper                
+
+@app.route("/login/", methods=["GET", "POST"])
+def login():
+    """ 
+    Si el usuario accede carga la página muestra el formulario, 
+    si no valida los datos del formulario para redirigir al home 
+    """
+
+    if request.method=="GET":           
+        return render_template('index.html', error=False)
+    elif request.method=="POST":
+        user=request.form['user']
+        password=request.form['password']
+        userfound=Database.run_sql(f"select * from users where user='{user}' AND password='{password}'")
+        if userfound:
+            session["user"]=user
+            return redirect(url_for('index'))
+        else:
+            return render_template('index.html', error=True)    
+
+@app.route("/")
+@validate_session()
+def index():
+    return render_template('selection.html')
+
+@app.route("/texas/", methods=["GET", "POST"])
+@validate_session()
+def texas():
+    user = session.get("user","")
+    userdata=Database.run_sql(f"select * from users where user='{user}'")[0]
+
+    if request.method=="GET":     
+        return render_template('texas.html', user=userdata["user"], counter=userdata["counter"])
+    elif request.method=="POST":
+        json_data = request.get_json()
+        pdfTexas(json_data["plate"], json_data["year"], json_data["make"], json_data["issue_date"], json_data["expiration_date"], json_data["vin"], json_data["major_color"], json_data["minor_color"], json_data["body"], json_data["model"], json_data["owner"], json_data["address"], json_data["city"], json_data["state"], json_data["zip_code"])
+        
+        # Get last counter
+        sql = f"""SELECT counter FROM users WHERE user = "{session['user']}";;"""
+        counter = Database.run_sql(sql)[0]["counter"]
+        counter += 1
+
+        # Update counter
+        sql = f"""UPDATE users SET counter = {counter} WHERE user = "{session['user']}";"""
+        Database.run_sql(sql)
+
+        return ({"ok":True},200)   
+
+@app.route("/california/", methods=["GET", "POST"])
+@validate_session()
+def california():
+    user = session.get("user","")
+    userdata=Database.run_sql(f"select * from users where user='{user}'")[0]
+
+    if request.method=="GET":     
+        return render_template('california.html', user=userdata["user"], counter=userdata["counter"])
+    elif request.method=="POST":
+        #TODO: generate plate
+        #TODO: devolver archivo pdf
+        return render_template('california.html', user=userdata["user"], counter=userdata["counter"])   
+    
+
+@app.route("/new_mexico/", methods=["GET", "POST"])
+@validate_session()
+def new_mexico():
+    user = session.get("user","")
+    userdata=Database.run_sql(f"select * from users where user='{user}'")[0]
+
+    if request.method=="GET":     
+        return render_template('new_mexico.html', user=userdata["user"], counter=userdata["counter"])
+    elif request.method=="POST":
+        #TODO: generate plate
+        #TODO: devolver archivo pdf
+        return render_template('new_mexico.html', user=userdata["user"], counter=userdata["counter"])
+
+@app.route("/logout/")
+@validate_session()
+def logout():
+    session.pop("user",None)
+    return redirect(url_for("login"))
+
+@app.route("/file/<filename>/")
+@validate_session()
+def file(filename):
+    file_path=os.path.join(os.path.dirname(__file__),"files",filename+".pdf")
+    with open(file_path, 'rb') as bites:
+        return send_file(
+            io.BytesIO(bites.read()),
+            download_name=filename+".pdf",
+            mimetype='application/pdf'
+        )
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=3000) 
---
+++ pdfmaker_Texas/PDFMaker.py
@@ -0,0 +1,156 @@
+import os
+import random
+import qrcode
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter, landscape
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.lib.colors import white, black
+from PyPDF2 import PdfFileMerger
+
+
+def generatePDF(plate, year, make, issue_date, expiration_date, vin, major_color, minor_color, body, model, owner, address, city, state, zip_code):
+	#plate = str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9)) + chr(random.randint(65, 90)) + str(random.randint(0, 9)) + str(random.randint(0, 9))
+
+	# File paths and fonts
+	current_folder = os.path.dirname (__file__)
+	parent_folder = os.path.dirname (current_folder)
+	files_folder = os.path.join (parent_folder, "files")
+	new_pdf_path = os.path.join (files_folder, f"{plate} newfile.pdf")
+	new_pdf2_path = os.path.join (files_folder, f"{plate} newfile2.pdf")
+
+	# Fonts with epeciufic path
+	pdfmetrics.registerFont(TTFont('sawarabi', os.path.join (current_folder, 'SawarabiGothic-Regular.ttf')))
+	pdfmetrics.registerFont(TTFont('oswald', os.path.join (current_folder, 'Oswald-Regular.ttf')))
+	pdfmetrics.registerFont(TTFont('oswald-semibold', os.path.join (current_folder, 'Oswald-SemiBold.ttf')))
+	pdfmetrics.registerFont(TTFont('oswald-bold', os.path.join (current_folder, 'Oswald-Bold.ttf')))
+	pdfmetrics.registerFont(TTFont('orion', os.path.join (current_folder, 'ORIOND.ttf')))
+	pdfmetrics.registerFont(TTFont('yantramanav', os.path.join (current_folder, 'Yantramanav-Regular.ttf')))
+	pdfmetrics.registerFont(TTFont('yantramanav-medium', os.path.join (current_folder, 'Yantramanav-Medium.ttf')))
+	pdfmetrics.registerFont(TTFont('yantramanav-bold', os.path.join (current_folder, 'Yantramanav-Bold.ttf')))
+	pdfmetrics.registerFont(TTFont('yantramanav-black', os.path.join (current_folder, 'Yantramanav-Black.ttf')))
+	pdfmetrics.registerFont(TTFont('clearsans-medium', os.path.join (current_folder, 'ClearSans-Medium.ttf')))
+	pdfmetrics.registerFont(TTFont('clearsans-bold', os.path.join (current_folder, 'ClearSans-Bold.ttf')))
+	pdfmetrics.registerFont(TTFont('texgy', os.path.join (current_folder, 'texgyreheros-bold.ttf')))
+	pdfmetrics.registerFont(TTFont('texgycn', os.path.join (current_folder, 'texgyreheroscn-bold.ttf')))
+	pdfmetrics.registerFont(TTFont('bierstadt', os.path.join (current_folder, 'Bierstadt.ttf')))
+	pdfmetrics.registerFont(TTFont('MS', os.path.join (current_folder, 'ms-reference-sans-serif.ttf')))
+	pdfmetrics.registerFont(TTFont('MSbd', os.path.join (current_folder, 'ms-reference-sans-serif-bold.ttf')))
+	pdfmetrics.registerFont(TTFont('times', os.path.join (current_folder, 'times.ttf')))
+	pdfmetrics.registerFont(TTFont('timesbd', os.path.join (current_folder, 'timesbd.ttf')))	
+	
+	c = canvas.Canvas(new_pdf_path, landscape(letter))
+	c.drawImage(os.path.join (current_folder, 'fondo.png'), 0, 0, 792, 612)
+	c.setFont('texgycn', 75)
+	c.drawString(195, 370, expiration_date)
+
+	data = f"http://127.0.0.1:5500/index.html?tagno=algo1&tagtype=algo2&effective-timestamp=algo3&verification-code=algo5&create-timestamp=algo6&end-timestamp=algo7&status-code=algo8&vin={vin}&year={year}&make=algo11&body=algo12&color=algo13&gdn=algo14&name=algo15&dba=algo16&address=algo17"
+	img = qrcode.make(data)
+	img.save(os.path.join(current_folder, 'QRCode.png'))
+
+	c.drawImage(os.path.join (current_folder, 'QRCode.png'), 605, 340, 110, 110)
+
+	c.setFont('orion', 160)
+	c.drawString(35, 225, plate)
+
+	c.setFont('texgycn', 34)
+	c.drawString(35, 192, year)
+	c.drawString(35, 160, make)
+
+	c.setFont('orion', 22)
+	c.drawString(475, 188, vin)
+	c.drawString(575, 168, 'TEXAS STAR')
+
+	c.setFillColor(white)
+	c.setFont('texgycn', 24)
+
+	distance = 438
+	for n in range(10):
+		if(n !=8):
+			c.drawString(753.5, distance, str(random.randint(0, 9)))
+		else:
+			c.drawString(753.5, distance, chr(random.randint(65, 90)))	
+		distance -= 30
+
+	c.setFillColor(black)
+	
+	c.showPage()
+	c.save()
+
+	c = canvas.Canvas(new_pdf2_path, letter)
+	c.drawImage(os.path.join (current_folder, 'logo_Tx.png'), 60, 205, 485.25, 486.75)
+	c.setFont('times', 16)
+	c.drawString(154, 732, "BUYER'S TAG RECEIPT - BUYER'S COPY")
+	c.setFont('times', 16)
+	c.drawString(154, 732, "BUYER'S TAG RECEIPT - BUYER'S COPY")
+	c.setFont('times', 12)
+	x = 36
+	y = 727
+	c.line(x, y, x + 535, y)
+	c.drawString(50, 708, "Tag Number: ")
+	c.drawString(50, 670, "Issue Date:")
+	c.drawString(50, 653, "VIN:")
+	c.drawString(50, 637, "Year:")
+	c.drawString(50, 621, "Make:")
+	c.drawString(50, 606, "Major Color:")
+	c.setFont('MS', 10)
+	c.drawString(178, 708, plate)
+	c.drawString(178, 670, issue_date)
+	c.drawString(178, 653, vin)
+	c.drawString(178, 637, year)
+	c.drawString(178, 621, make)
+	c.drawString(178, 606, major_color)
+	c.setFont('times', 12)
+	c.drawString(308, 708, "Date of Sale: ")
+	c.drawString(308, 692, "Expiration Date:")
+	c.drawString(308, 637, "Body Style:")
+	c.drawString(308, 621, "Model:")
+	c.drawString(308, 606, "Minor Color:")
+	c.setFont('MS', 10)
+	c.drawString(438, 708, issue_date)
+	c.drawString(438, 692, expiration_date)
+	c.drawString(438, 637, body)
+	c.drawString(438, 621, model)
+	c.drawString(438, 606, minor_color)
+	c.setFont('times', 12)
+	c.drawString(92, 569, "Remarks:")
+	c.setFont('MS', 10)
+	c.drawString(92, 555, "ACTUAL MILEAGE")
+	c.drawString(92, 542, "CCO ISSUED:  [04/22/2019]")
+	c.drawString(92, 527, "PAPER TITLE")
+	c.drawString(92, 512, "E-REMINDER & PAPER RENEWAL NOTICE")
+	c.setFont('times', 12)
+	c.drawString(92, 482, "Issuing Dealer:")
+	c.drawString(92, 465, "Dealer Number:")
+	c.drawString(92, 434, "Purchaser")
+	c.drawString(92, 418, "Name 1:")
+	c.drawString(92, 403, "Address:")
+	c.drawString(127, 312, "BUYER is required to keep this receipt in the vehicle until vehicle is")
+	c.drawString(127, 294, "registered and metal plates are placed on the vehicle.")
+	c.setFont('MS', 10)
+	c.drawString(308, 482, "FIESTA MOTORS INC")
+	c.drawString(308, 465, "594870R")
+	c.drawString(308, 418, owner)
+	c.drawString(308, 403, address)
+	c.drawString(308, 388, city+", "+state)
+	c.drawString(308, 373, zip_code)
+	c.setFont('times', 16)
+	c.drawString(249, 258, "BUYER'S COPY")
+	c.save()
+	
+	pdfs = [new_pdf_path, new_pdf2_path]
+
+	merger = PdfFileMerger()
+
+	for pdf in pdfs:
+		merger.append(pdf)
+
+	plate_path = os.path.join (files_folder, plate+".pdf")
+	merger.write(plate_path)
+	merger.close()
+
+	os.remove (new_pdf_path)
+	os.remove (new_pdf2_path)
+
+if __name__=='__main__':
+	generatePDF("2179W46", "2019", "CHEVROLET", "DEC 02, 2020", "FEB 20, 2023", "1GCUYDED8KZ135075", "RED", "GREEN", "TRUCK", "FORD", "Someone", "Someplace", "City", "State", "20000")
