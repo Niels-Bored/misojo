44a806e6db63565b7dcaad59d249927b5cb72aae
AlbertoSnchez <75083926+AlbertoSnchez@users.noreply.github.com>
2023-11-08 20:50:27 -0600

    Navegación con tab

--- static/JS/index.js
+++ static/JS/index.js
@@ -10,17 +10,37 @@ document.getElementById("inpUser").addEventListener("mouseover", ()=>{
     talk("Entrada usuario");
 });
 
+document.getElementById("inpUser").addEventListener("keyup", (e)=> {
+    if( e.which == 9 ) {
+        talk("Entrada usuario");
+}});
+
 document.getElementById("inpPass").addEventListener("mouseover", ()=>{
     talk("Entrada Contraseña");
 });
+
+document.getElementById("inpPass").addEventListener("keyup", (e)=> {
+    if( e.which == 9 ) {
+        talk("Entrada Contraseña");
+}});
 
 document.getElementById("btnsubmit").addEventListener("mouseover", ()=>{
     talk("Enviar consulta");
 });
 
+document.getElementById("btnsubmit").addEventListener("keyup", (e)=> {
+    if( e.which == 9 ) {
+        talk("Enviar consulta");
+}});
+
 document.getElementById("btnsignup").addEventListener("mouseover", ()=>{
     talk("Registrarse");
 });
+
+document.getElementById("btnsignup").addEventListener("keyup", (e)=> {
+    if( e.which == 9 ) {
+        talk("Registrarse");
+}});
 
 function talk(text) {
     const utterance = new SpeechSynthesisUtterance(text);
