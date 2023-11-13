ec080661b0cd8eb3cd081ee6f846e5ceafd9ae03
AlbertoSnchez <75083926+AlbertoSnchez@users.noreply.github.com>
2023-11-12 18:54:28 -0600

    Reproductor listo

--- static/JS/player.js
+++ static/JS/player.js
@@ -11,3 +11,80 @@ function nextPage(filename, page){
     page++
     window.location.href='/player/'+filename+"/"+page
 }
+
+const h3 = document.querySelector("h3");
+
+h3.addEventListener("mouseover", ()=>{
+    talk(h3.textContent);
+});
+
+h3.addEventListener("keyup", (e)=> {
+    if( e.which == 9 ) {
+        talk(h3.textContent);
+}});
+
+const ps = document.querySelectorAll("p");
+
+for (const p of ps) {
+    p.addEventListener("mouseover", ()=>{
+        talk(p.textContent);
+    });
+
+    p.addEventListener("keyup", (e)=> {
+        if( e.which == 9 ) {
+        console.log("p tag");
+
+            talk(p.textContent);
+        }
+    });
+}
+
+const a = document.querySelector("a");
+
+a.addEventListener("mouseover", ()=>{
+    talk(a.textContent);
+});
+
+a.addEventListener("keyup", (e)=> {
+    if( e.which == 9 ) {
+        talk(a.textContent);
+}});
+
+function talk(text) {
+    console.log(text);
+    const utterance = new SpeechSynthesisUtterance(text);
+    utterance.lang = 'es-MX';
+    speechSynthesis.speak(utterance);
+}
+
+const audio = document.querySelector('audio');
+const playButton = document.querySelector('#btnplay');
+const stopButton = document.querySelector('#btnstop');
+
+playButton.addEventListener('click', () => {
+  if (audio.paused) {
+    audio.play();
+  } else {
+    audio.pause();
+  }
+});
+
+stopButton.addEventListener('click', () => {
+  audio.pause();
+});
+
+const buttons = document.querySelectorAll("button");
+
+for (const button of buttons) {
+    button.addEventListener("mouseover", ()=>{
+        talk(button.textContent);
+    });
+
+    button.addEventListener("keyup", (e)=> {
+        if( e.which == 9 ) {
+        console.log("p tag");
+
+            talk(button.textContent);
+        }
+    });
+}
